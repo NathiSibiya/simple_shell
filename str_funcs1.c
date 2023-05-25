3ef6847c610385b095e5bfca9a35f2c16dc1393a
cutifavour <cuti.favour@yahoo.com>
2023-05-24 21:51:09 -0700

    my shell project done

--- str_funcs1.c
+++ str_funcs1.c
@@ -23,6 +23,24 @@ int _strlen(const char *s)
 }
 
 /**
+ * _strcpy - this copies the string pointed to by src, including the
+ *           terminating null byte, to the buffer pointed by des.
+ * @dest: is a pointer to the destination of copied string.
+ * @src: is a pointer to the src of the source string.
+ *
+ * Return: a pointer to dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * _strcat - this concantenates two strings.
  * @dest: this is a pointer to destination string.
  * @src: this is a pointer to source string.
